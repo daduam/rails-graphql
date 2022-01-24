b8a743a5a1fae1e5fa1c834232899d95727cf229
Joseph Ampadu <josephampadu549@gmail.com>
2022-01-24 00:29:04 +0000

    graphql setup

--- tut-howtographql/config/routes.rb
+++ tut-howtographql/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
