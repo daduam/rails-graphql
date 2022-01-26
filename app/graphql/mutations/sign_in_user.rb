6d49b57fd7cd6e858c6c9bcadcfdd8cc1d67b2ab
Joseph Ampadu <josephampadu549@gmail.com>
2022-01-26 01:39:05 +0000

    authentication with graphql

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -16,7 +16,9 @@ module Mutations
       return unless user.authenticate(credentials[:password])
 
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-      token = crypt.encrypt_and_sign("user-id#{user.id}")
+      token = crypt.encrypt_and_sign("user-id:#{user.id}")
+
+      context[:session][:token] = token
 
       {token: token, user: user}
     end
